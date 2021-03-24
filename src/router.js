38a71b88b36f1cef9194d23603e82f3d193064cb
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-24 16:14:51 +0900

    add to main.js

--- src/router.js
+++ src/router.js
@@ -1,11 +1,58 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import Index from "./components/pages/index";
+import About from "./components/pages/about";
+import Detail from "./components/pages/detail/index";
+import Nest from "./components/pages/index";
+import Index from "./components/pages/nest/parent";
+import Child1 from "./components/pages/nest/child1";
+import Child2 from "./components/pages/nest/child2";
+import Child3 from "./components/pages/nest/child3";
+
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   routes: [
     //ルーティングの設定
+    {
+      path: '/',
+      component: Index
+    },
+    {
+      path: '/about',
+      component: About,
+    },
+    {
+      path: '/about',
+      component: About,
+    },
+    {
+      path: '/detail',
+      component: Detail,
+    },
+    {
+      path: "detail/:id",
+      component: DetailId
+    },
+    {
+      path: '/nest',
+      component: Nest,
+      children: [
+        {
+          path: "",
+          component: Child1
+        },
+        {
+          path: 'child2',
+          component: Child2,
+        },
+        {
+          path: 'child3',
+          component: Child3
+        }
+      ]
+    }
   ]
-})
+});
