a53d547212ad11e95a2fcb6c0e190f8c8b8d1df5
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-24 17:19:16 +0900

    add to detail/index.vue

---
+++ src/components/pages/detail/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>This is Detail Page!!</h1>
+    <ul>
+      <li class="detail-item" v-for="l in list" :key="l.id" @click="mobe(l.id)">
+        <span>{{ l.id }} {{ l.name }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        list: [
+          { id: "001", name: "Aさん" },
+          { id: "002", name: "Bさん" },
+          { id: "003", name: "Cさん" },
+          { id: "004", name: "Dさん" },
+          { id: "005", name: "Eさん" },
+        ]
+      };
+    },
+    methods: {
+      move(id) {
+        this.$router.push(`/detail/${id}`);
+      }
+    }
+  };
+</script>
+
+<style>
+.detail-item{
+  cursor: pointer;
+  line-height: 40px;
+}
+
+.detail-item > span{
+  text-decoration: underline;
+}
+.detail-item > span:hover{
+  opacity: 0.8;
+}
+</style>
