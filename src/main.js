f5d6fdaf8dd4bbf9c42a4c1425287521e295da24
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-24 22:30:46 +0900

    edit main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
+// import Vue from 'vue';
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from "./router";
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+// new Vue({
+//   router,
+//   render: h => h(App)
+// })
+
+createApp(App).mount('#app')
+createApp(Router).mount('#app')
