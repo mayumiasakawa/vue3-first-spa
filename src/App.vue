cb32e03055464e04ea4919e9c3a3225a14e44e7e
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-24 16:24:29 +0900

    add to App.vue style

--- src/App.vue
+++ src/App.vue
@@ -30,5 +30,53 @@ export default{
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 
+#app {
+  margin: 0;
+  padding: 0;
+}
+
+.links {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  list-style: none;
+}
+
+.links > li a:hover {
+  background: #f6f6f6;
+}
+
+.links > li > a.router-link-active {
+  background: ransparent;
+  color: teal;
+  cursor: defalt;
+}
+
+.page {
+  position: relative;
+}
+
+.page > *{
+  position: absolute;
+}
+
+.router-enter-active{
+  transition: all 1s;
+  transition-delay: 0.5s;
+}
+
+.router-leave-active{
+  transition: all 0.5s;
+}
+
+.router-enter,
+.router-leave-to{
+  opacity: 0;
+}
 </style>
