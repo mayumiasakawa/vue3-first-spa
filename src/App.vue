6db5fdae3643d85ecfd37724ea6628408c2b54cb
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-24 16:20:20 +0900

    add to App.vue tempolate

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
+  <div id="app">
+    <ul class="links">
+      <li>
+        <router-link to="/" exact>HOME</router-link>
+      </li>
+      <li>
+        <router-link to="/about">ABOUT</router-link>
+      </li>
+      <li>
+        <router-link to="/detail">DETAIL</router-link>
+      </li>
+      <li>
+        <router-link to="/nest">NEST</router-link>
+      </li>
+    </ul>
 
+    <div class="page">
+      <transition name="router">
+        <roure-view/>
+      </transition>
+    </div>
+  </div>
 </template>
 
 <script>
